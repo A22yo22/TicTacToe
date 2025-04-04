b6320509ffe5aefce585adf808b407a23a05ee5d
UnNamedDev <a22yo22io@gmail.com>
2025-04-04 12:36:55 +0200

    Added Win condition checking

--- win_conditions.py
+++ win_conditions.py
@@ -1,44 +1,64 @@
 class winConditions:
-    allWinOptions = {
-        0: [1, 1, 1,
-            0, 0, 0,
-            0, 0, 0],
-
-        1: [0, 0, 0,
-            1, 1, 1,
-            0, 0, 0],
-
-        2: [0, 0, 0,
-            0, 0, 0,
-            1, 1, 1],
-            
-        2: [0, 0, 0,
-            0, 0, 0,
-            1, 1, 1],
-            
-        3: [1, 0, 0,
-            1, 0, 0,
-            1, 0, 0],
-            
-        4: [0, 1, 0,
-            0, 1, 0,
-            0, 1, 0],
-            
-        5: [0, 0, 1,
-            0, 0, 1,
-            0, 0, 1],
-            
-        6: [1, 0, 0,
-            0, 1, 0,
-            0, 0, 1],
-            
-        7: [0, 0, 1,
-            0, 1, 0,
-            1, 0, 0],
-    }
-
-    def FilterPlayer(playerToFilter):
-        return
-
     def CheckBoard(board):
+        #chck line by line not whole board
+        # i=0
+        allWinOptions = {
+            0: [1, 1, 1,
+                0, 0, 0,
+                0, 0, 0],
+
+            1: [0, 0, 0,
+                1, 1, 1,
+                0, 0, 0],
+
+            2: [0, 0, 0,
+                0, 0, 0,
+                1, 1, 1],
+                
+            2: [0, 0, 0,
+                0, 0, 0,
+                1, 1, 1],
+                
+            3: [1, 0, 0,
+                1, 0, 0,
+                1, 0, 0],
+                
+            4: [0, 1, 0,
+                0, 1, 0,
+                0, 1, 0],
+                
+            5: [0, 0, 1,
+                0, 0, 1,
+                0, 0, 1],
+                
+            6: [1, 0, 0,
+                0, 1, 0,
+                0, 0, 1],
+                
+            7: [0, 0, 1,
+                0, 1, 0,
+                1, 0, 0],
+        }
+
+        for key in allWinOptions:
+            
+            currentBorad = allWinOptions[key]
+            # print(allWinOptions[key])
+            # print(allWinOptions[key][0])
+            # print("----------")
+            
+            matchesFound = 0
+            for i in range(9):
+                # print(i)
+                
+                if((board[i] + currentBorad[i]) == 2):
+                    # print(allWinOptions[key][i])
+                    matchesFound += 1
+
+                i += 1
+            
+            print(matchesFound)
+
         return
+    
+    CheckBoard([1, 1, 1, 0,0,0,0,0,0])
